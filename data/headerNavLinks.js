8e43a969a9135cbb1dfa65174fe84221f3ee3868
PeterHassaballah <peter.hassaballah@gmail.com>
2022-08-06 21:52:59 +0200

    [fix] edited navbar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,10 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/blog', title: 'What We Do' },
   { href: '/projects', title: 'Partners in Success' },
-  { href: '/about', title: 'About' },
+  { href: '/tags', title: 'Ad Spaces' },
+  { href: '/about', title: 'Contact Us' },
+  // { href: '/about', title: 'Sign Up' },
+  // { href: '/about', title: 'Log In' },
 ]
 
 export default headerNavLinks
