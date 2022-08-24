eba15229140c62267d5baec7d3cfef1e7e65c213
PeterHassaballah <peter.hassaballah@gmail.com>
2022-08-24 21:23:49 +0200

    [feat] added images to ad spaces avatars

--- components/AntCard.jsx
+++ components/AntCard.jsx
@@ -1,7 +1,6 @@
 import { Card, Row, Col, Avatar } from 'antd'
 import React from 'react'
 import 'antd/dist/antd.css'
-import { AntDesignOutlined } from '@ant-design/icons'
 
 const AntCard = () => {
   const onChange = (currentSlide) => {
@@ -19,17 +18,52 @@ const AntCard = () => {
             <Card.Grid className="bg-[#C0FFEE] text-center">
               <Avatar
                 size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-                icon={<AntDesignOutlined />}
+                src="https://images.pexels.com/photos/2422404/pexels-photo-2422404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               />
               <br></br>
               Billboards
             </Card.Grid>
-            <Card.Grid className="text-center">Digital Screens</Card.Grid>
-            <Card.Grid className="text-center">Transit Media</Card.Grid>
+            <Card.Grid className="text-center">
+              <Avatar
+                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                src="https://images.pexels.com/photos/1486222/pexels-photo-1486222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <br></br>
+              Digital Screens
+            </Card.Grid>
+            <Card.Grid className="text-center">
+              <Avatar
+                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                src="https://scontent.fcai16-1.fna.fbcdn.net/v/t39.30808-6/288381481_1431225163990971_3641542336354012229_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=PDVG2f8OjnMAX93oGdo&_nc_ht=scontent.fcai16-1.fna&oh=00_AT9Q_e1gST6qU7Nryb1kktB9ZITvbTj27JNwGkFCDoALCQ&oe=630C1371"
+              />
+              <br></br>
+              Transit Media
+            </Card.Grid>
 
-            <Card.Grid className="text-center">National Parks</Card.Grid>
-            <Card.Grid className="text-center">Restaurants</Card.Grid>
-            <Card.Grid className="text-center">Power Stations and Substations</Card.Grid>
+            <Card.Grid className="text-center">
+              <Avatar
+                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                src="https://images.pexels.com/photos/258420/pexels-photo-258420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <br></br>
+              Metro{' '}
+            </Card.Grid>
+            <Card.Grid className="text-center">
+              <Avatar
+                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                src="https://images.pexels.com/photos/1661496/pexels-photo-1661496.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <br></br>
+              Street
+            </Card.Grid>
+            <Card.Grid className="text-center">
+              <Avatar
+                size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                src="https://images.pexels.com/photos/6253/city-street-typography-design.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <br></br>
+              Place Based Media
+            </Card.Grid>
           </Card>
         </Col>
       </Row>
